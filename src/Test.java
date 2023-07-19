2b460cfa3b08b8594ce21df7aa3c105bd9237d4b
parth251 <139961631+parth251@users.noreply.github.com>
2023-07-19 14:30:13 +0530

    my code

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list=new LinkedList();
+		list.add(10);
+		list.add(12);
+		list.add(13);
+		for (Integer o:list)
+		{
+			System.out.println(o+1);
+		}
+	}
+	
+}
